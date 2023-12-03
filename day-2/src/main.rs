4b2a09e43411b057b507af5c10578149c6394838
Paul Zhang <me@paul-zhang.de>
2023-12-03 15:21:17 +0100

    Fix test for day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -145,11 +145,13 @@ mod test {
     #[test]
     fn test_calculate_result_puzzle_part_1() -> eyre::Result<()> {
         let res = calculate_result(
-            r"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            "\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+",
             PuzzlePart::One,
         )?;
         assert_eq!(res, 8);
@@ -160,12 +162,14 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
     #[test]
     fn test_calculate_result_puzzle_part_2() -> eyre::Result<()> {
         let res = calculate_result(
-            r"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            "\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-            PuzzlePart::One,
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+",
+            PuzzlePart::Two,
         )?;
         assert_eq!(res, 2286);
 
